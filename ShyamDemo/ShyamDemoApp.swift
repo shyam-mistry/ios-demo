6ce8fe17c992c97df7b104cc38b667fe628050a2
shyam mistry <shyam.mistry@xero.com>
2022-03-29 15:57:48 +0100

    Initial Commit

---
+++ ShyamDemo/ShyamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShyamDemoApp.swift
+//  ShyamDemo
+//
+//  Created by Shyam Mistry on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShyamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShyamDemoUITests/ShyamDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShyamDemoUITestsLaunchTests.swift
+//  ShyamDemoUITests
+//
+//  Created by Shyam Mistry on 29/03/2022.
+//
+
+import XCTest
+
+class ShyamDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
